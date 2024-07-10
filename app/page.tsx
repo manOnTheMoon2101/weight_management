f1edcc12e07d59e5b882c8d9b961a7bf86a67acb
Cleve Clayton <95036334+manOnTheMoon2101@users.noreply.github.com>
2024-07-10 16:53:15 +0200

    changed naming of body_header main file

--- app/page.tsx
+++ app/page.tsx
@@ -4,14 +4,14 @@ import Image from "next/image";
 import axios from "axios";
 import Header from "./components/title_header/Header";
 import Dashboard from "./components/dashboard/Dashboard";
-import Filter from "./components/body_header/Filter";
+import Body_Header from "./components/body_header/main";
 
 export default function Home(e: any) {
   return (
     <main>
       <Header />
       <div>
-        <Filter />
+        <Body_Header />
       </div>
       <div>
         <Dashboard></Dashboard>
