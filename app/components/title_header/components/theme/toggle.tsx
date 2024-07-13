a3262a26c0bc149b068649e7d9e3366d3d40a802
Cleve Clayton <95036334+manOnTheMoon2101@users.noreply.github.com>
2024-07-13 12:53:33 +0200

    hydration issue fixed(mode/theme returns error)

--- app/components/title_header/components/theme/toggle.tsx
+++ app/components/title_header/components/theme/toggle.tsx
@@ -12,6 +12,7 @@ import {
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
 import { Switch } from "@/components/ui/switch";
+import { Label } from "@/components/ui/label";
 import { useEffect } from "react";
 export function ModeToggle() {
   const { theme, setTheme } = useTheme();
@@ -27,31 +28,10 @@ export function ModeToggle() {
     }
   }, []);
 
-  
+  const mode = theme ? "light" : theme ? "dark" : null
 
   return (
     <div>
-      {/* <DropdownMenu>
-        <DropdownMenuTrigger asChild>
-          <Button variant="outline" size="icon">
-            <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-            <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
-            <span className="sr-only">Toggle theme</span>
-          </Button>
-        </DropdownMenuTrigger>
-        <DropdownMenuContent align="end">
-          <DropdownMenuItem onClick={() => setTheme("light")}>
-            Light
-          </DropdownMenuItem>
-          <DropdownMenuItem onClick={() => setTheme("dark")}>
-            Dark
-          </DropdownMenuItem>
-          <DropdownMenuItem onClick={() => setTheme("system")}>
-            System
-          </DropdownMenuItem>
-        </DropdownMenuContent>
-      </DropdownMenu> */}
-<p>mode:{theme}</p>
       <Switch onCheckedChange={handleThemeToggle} />
     </div>
   );
