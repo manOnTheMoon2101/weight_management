da9addc38d1d3a5ffe9b721da35383e789865bd5
Cleve Clayton <95036334+manOnTheMoon2101@users.noreply.github.com>
2024-07-13 10:40:35 +0200

    error fix

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { Dashboard } from "./components/body_header/dashboard/Dashboard";
+import { Dashboard } from "./components/body/dashboard/Dashboard";
 import Header from "./components/title_header/Header";
 export default function Home(e: any) {
   return (
