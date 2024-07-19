bd97833efa27b4e557cb855d1d811fbaa19c0a47
Cleve Clayton <95036334+manOnTheMoon2101@users.noreply.github.com>
2024-07-19 20:58:06 +0200

    removed unnused packages

--- app/components/title_header/components/account/account.tsx
+++ app/components/title_header/components/account/account.tsx
@@ -9,7 +9,7 @@ import {
   DropdownMenuItem,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
-import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { Avatar, AvatarFallback} from "@/components/ui/avatar";
 
 import { signIn, signOut } from "next-auth/react";
 const Account = () => {
