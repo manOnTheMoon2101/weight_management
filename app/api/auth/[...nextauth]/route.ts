38635da391849f4b73c2873f910d0f31af3e9ba2
Cleve Clayton <95036334+manOnTheMoon2101@users.noreply.github.com>
2024-07-20 18:01:38 +0200

    url change

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,7 +1,5 @@
-import prisma from "@/prisma/prisma";
-import { compare } from "bcrypt";
+
 import NextAuth, { type NextAuthOptions } from "next-auth";
-import CredentialsProvider from "next-auth/providers/credentials";
 import { authOptions } from "@/app/auth/options";
 
 const handler = NextAuth(authOptions);
