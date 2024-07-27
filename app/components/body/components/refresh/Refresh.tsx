90b4001bbfc94bb1c56fb4056cc02d3a199d863c
Cleve Clayton <95036334+manOnTheMoon2101@users.noreply.github.com>
2024-07-27 19:32:12 +0200

    little styling to login

--- app/components/body/components/refresh/Refresh.tsx
+++ app/components/body/components/refresh/Refresh.tsx
@@ -3,7 +3,7 @@ import { IoMdRefresh } from "react-icons/io";
 const Refresh = () => {
   return (
     <div>
-      <IoMdRefresh size={25} className="cursor-pointer" />
+      {/* <IoMdRefresh size={25} className="cursor-pointer animate-spin" /> */}
     </div>
   );
 };
--- app/login/page.tsx
+++ app/login/page.tsx
@@ -5,6 +5,7 @@ import { Button } from "@/components/ui/button";
 import Link from "next/link";
 import { signIn } from "next-auth/react";
 import { useRef } from "react";
+import { Label } from "@/components/ui/label";
 const Login = () => {
   const email = useRef("");
   const password = useRef("");
@@ -19,25 +20,36 @@ const Login = () => {
   };
 
   return (
-    <div className="min-h-screen flex items-center justify-center">
-      <div className="p-8 rounded shadow-md">
+    <div className="min-h-screen flex flex-col items-center justify-center">
+      <div>
+        <h2>
+          Welcome to <span>Weight Management</span>
+        </h2>
+      </div>
+      <div className="p-8 rounded shadow-md border-4 rounded">
+        <Label>Email</Label>
         <Input
-          placeholder="Email"
+          placeholder="email@email.com"
           type="email"
           onChange={(e: any) => {
             email.current = e.target.value;
           }}
         />
+        <Label>Password</Label>
         <Input
-          placeholder="Password"
+          placeholder="*****"
           type="password"
           onChange={(e: any) => {
             password.current = e.target.value;
           }}
         />
-        <p>
-          <Link href="/register">Register</Link>
-        </p>
+
+        <div className="flex flex-row justify-end">
+          <Link href="/register" className="underline text-xs">
+            No Account?Create one now
+          </Link>
+        </div>
+
         <Button onClick={onSubmit}>Login</Button>
       </div>
     </div>
