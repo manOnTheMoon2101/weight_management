d716e506ed7f084987a228716f69db51e8e038b4
Cleve Clayton <95036334+manOnTheMoon2101@users.noreply.github.com>
2024-07-27 12:18:05 +0200

    removed scraper

--- app/api/scrape/route.tsx
+++ app/api/scrape/route.tsx
@@ -1,44 +1,44 @@
-import { NextResponse } from 'next/server';
-import { PrismaClient } from '@prisma/client';
-import scrapeData from '../../../services/data.json';
+// import { NextResponse } from 'next/server';
+// import { PrismaClient } from '@prisma/client';
+// import scrapeData from '../../../services/data.json';
 
-const prisma = new PrismaClient();
+// const prisma = new PrismaClient();
 
-export const POST = async () => {
-  try {
-    for (const food of scrapeData) {
+// export const POST = async () => {
+//   try {
+//     for (const food of scrapeData) {
 
-      //old data
-      // await prisma.food.create({
-      //   data: {
-      //     name: food.name,
-      //     calories: food.calories,
-      //     sugar: food.sugar,
-      //     carbs: food.carbs,
+//       //old data
+//       // await prisma.food.create({
+//       //   data: {
+//       //     name: food.name,
+//       //     calories: food.calories,
+//       //     sugar: food.sugar,
+//       //     carbs: food.carbs,
   
-      //   },
-      // });
+//       //   },
+//       // });
 
 
-          // Update existing data
-    // for (const food of scrapeData) {
-    //   await prisma.food.updateMany({
-    //     where: {
-    //       name: food.name,
-    //     },
-    //     data: {
-    //       protein: food.protein, // Update protein field
-    //       iron: food.iron,       // Update iron field
-    //     },
-    //   });
-    // }
+//           // Update existing data
+//     // for (const food of scrapeData) {
+//     //   await prisma.food.updateMany({
+//     //     where: {
+//     //       name: food.name,
+//     //     },
+//     //     data: {
+//     //       protein: food.protein, // Update protein field
+//     //       iron: food.iron,       // Update iron field
+//     //     },
+//     //   });
+//     // }
 
-    }
+//     }
 
-    await prisma.$disconnect();
-    return NextResponse.json({ message: "Data inserted successfully" });
-  } catch (err) {
-    await prisma.$disconnect();
-    return NextResponse.json({ message: "POST Error", err }, { status: 500 });
-  }
-};
+//     await prisma.$disconnect();
+//     return NextResponse.json({ message: "Data inserted successfully" });
+//   } catch (err) {
+//     await prisma.$disconnect();
+//     return NextResponse.json({ message: "POST Error", err }, { status: 500 });
+//   }
+// };
