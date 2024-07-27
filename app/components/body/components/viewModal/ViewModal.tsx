b30a0a297ee491fa35ed5ce53d2145db6878f258
Cleve Clayton <95036334+manOnTheMoon2101@users.noreply.github.com>
2024-07-28 01:11:11 +0200

    nessecary fields for viewmodal

--- app/components/body/components/viewModal/ViewModal.tsx
+++ app/components/body/components/viewModal/ViewModal.tsx
@@ -7,7 +7,22 @@ const ViewModal = (data: any) => {
     <div>
       <DialogTitle>{data.x.createdAt}</DialogTitle>
       <DialogDescription>weight:{data.x.weight}</DialogDescription>
-
+      <DialogDescription>calories:{data.x.totalCalories}</DialogDescription>
+      <DialogDescription>protein:{data.x.totalProtein}</DialogDescription>
+      <DialogDescription>fat:{data.x.totalFat}</DialogDescription>
+      <DialogDescription>carbs:{data.x.totalCarbs}</DialogDescription>
+      <DialogDescription>
+        sugar:{data.x.totalSugar ? "true" : "false"}
+      </DialogDescription>
+      <DialogDescription>
+        vitamin:{data.x.tookVitamin ? "true" : "false"}
+      </DialogDescription>
+      <DialogDescription>
+        fat burner:{data.x.tookFatburner ? "true" : "false"}
+      </DialogDescription>
+      <DialogDescription>
+        cla:{data.x.tookWeightmanagement ? "true" : "false"}
+      </DialogDescription>
       <DeleteButton data={data.x.id} />
       <EditButton />
     </div>
