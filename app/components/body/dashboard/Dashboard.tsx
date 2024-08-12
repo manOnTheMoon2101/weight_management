3f318e9d562378b9b401f9469d92925430a45b89
Cleve Clayton <95036334+manOnTheMoon2101@users.noreply.github.com>
2024-08-12 17:25:04 +0200

    margin to mobile title components

--- app/components/body/dashboard/Dashboard.tsx
+++ app/components/body/dashboard/Dashboard.tsx
@@ -30,32 +30,36 @@ export function Dashboard() {
   return (
     <div>
       <div className="flex flex-row justify-between">
-        <Select
-          value={selectedMonth}
-          onValueChange={(value) => {
-            setSelectedMonth(value);
-          }}
-          defaultValue={getCurrentMonthTwoDigit()}
-        >
-          <SelectTrigger className="w-[180px]">
-            <SelectValue placeholder="Select Month" />
-          </SelectTrigger>
-          <SelectContent>
-            <SelectItem value="01">January</SelectItem>
-            <SelectItem value="02">February</SelectItem>
-            <SelectItem value="03">March</SelectItem>
-            <SelectItem value="04">April</SelectItem>
-            <SelectItem value="05">May</SelectItem>
-            <SelectItem value="06">June</SelectItem>
-            <SelectItem value="07">July</SelectItem>
-            <SelectItem value="08">August</SelectItem>
-            <SelectItem value="09">September</SelectItem>
-            <SelectItem value="10">October</SelectItem>
-            <SelectItem value="11">November</SelectItem>
-            <SelectItem value="12">December</SelectItem>
-          </SelectContent>
-        </Select>
-        <AddForm />
+        <div className="mx-2 md:">
+          <Select
+            value={selectedMonth}
+            onValueChange={(value) => {
+              setSelectedMonth(value);
+            }}
+            defaultValue={getCurrentMonthTwoDigit()}
+          >
+            <SelectTrigger className="w-[180px]">
+              <SelectValue placeholder="Select Month" />
+            </SelectTrigger>
+            <SelectContent>
+              <SelectItem value="01">January</SelectItem>
+              <SelectItem value="02">February</SelectItem>
+              <SelectItem value="03">March</SelectItem>
+              <SelectItem value="04">April</SelectItem>
+              <SelectItem value="05">May</SelectItem>
+              <SelectItem value="06">June</SelectItem>
+              <SelectItem value="07">July</SelectItem>
+              <SelectItem value="08">August</SelectItem>
+              <SelectItem value="09">September</SelectItem>
+              <SelectItem value="10">October</SelectItem>
+              <SelectItem value="11">November</SelectItem>
+              <SelectItem value="12">December</SelectItem>
+            </SelectContent>
+          </Select>
+        </div>
+        <div className="mx-2 md:">
+          <AddForm />
+        </div>
       </div>
       <div className="flex flex-col md:flex-row justify-around m-5 h-full">
         <div className="w-full my-5 md:w-[30%] max-h-[50vh] overflow-auto mx-2 ">
