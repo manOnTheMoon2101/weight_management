8e3b6072607756fae01081da76c9a13f22291fa2
Cleve Clayton <95036334+manOnTheMoon2101@users.noreply.github.com>
2024-08-29 19:52:27 +0200

    removed function

--- app/components/body/components/addform/AddForm.tsx
+++ app/components/body/components/addform/AddForm.tsx
@@ -512,19 +512,3 @@ export function AddForm() {
     </Drawer>
   );
 }
-
-function ProfileForm({ className }: React.ComponentProps<"form">) {
-  return (
-    <form className={cn("grid items-start gap-4", className)}>
-      <div className="grid gap-2">
-        <Label htmlFor="email">Email</Label>
-        <Input type="email" id="email" defaultValue="shadcn@example.com" />
-      </div>
-      <div className="grid gap-2">
-        <Label htmlFor="username">Username</Label>
-        <Input id="username" defaultValue="@shadcn" />
-      </div>
-      <Button type="submit">Save changes</Button>
-    </form>
-  );
-}
