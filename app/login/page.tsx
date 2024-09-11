e1380d59987de1665a35b96be189bfca6f4ef646
Cleve Clayton <95036334+manOnTheMoon2101@users.noreply.github.com>
2024-09-11 20:18:30 +0200

    Drawer/Dialog to info

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -7,9 +7,9 @@ import { signIn } from "next-auth/react";
 import { useState } from "react";
 import { AiOutlineLoading } from "react-icons/ai";
 import { Label } from "@/components/ui/label";
-import { FaInfoCircle } from "react-icons/fa";
 import { useRouter } from "next/navigation";
 import isValidEmail from "../utils/emailValidation";
+import { Info } from "./components/info/Info";
 import { FaEye, FaEyeSlash } from "react-icons/fa";
 import {
   Popover,
@@ -51,38 +51,7 @@ const Login = () => {
     <div className="min-h-screen flex flex-col items-center justify-center">
       <div className="p-8">
         <div className="flex flex-row justify-end">
-          <Popover>
-            <PopoverTrigger asChild>
-              <Button className="cursor-help" variant="ghost">
-                <FaInfoCircle size={30} className="text-orange-400" />
-              </Button>
-            </PopoverTrigger>
-            <PopoverContent className="w-80 h-80 max-w-full max-h-full overflow-auto">
-              <div>
-                <h2 className="text-center text-2xl">
-                  Welcome to my Weight Management App!
-                </h2>
-                <p className="m-5 text-center ">
-                  This platform is designed specifically for showcasing
-                  individual work and is used by a select group of users.
-                  <br />
-                  <br />
-                  To ensure the security of your information, please avoid using
-                  real-life Google passwords or any other sensitive credentials
-                  when registering or logging in.
-                  <br />
-                  <br />
-                </p>
-                <p className="m-5 text-center">
-                  If you encounter any bugs or issues while using the site,I
-                  encourage you to report them so we can address them promptly.
-                  <br />
-                  Your feedback is invaluable in helping me improve the
-                  experience for everyone.
-                </p>
-              </div>
-            </PopoverContent>
-          </Popover>
+      <Info/>
         </div>
         <Label className="text-4xl text-center">Email</Label>
         <Input
