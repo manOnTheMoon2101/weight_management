c3d910cbb1a5042e1635f8db64151fdc014c41e6
Cleve Clayton <95036334+manOnTheMoon2101@users.noreply.github.com>
2024-09-25 20:54:05 +0200

    error fix

--- app/components/title_header/components/account/account.tsx
+++ app/components/title_header/components/account/account.tsx
@@ -1,19 +1,6 @@
 "use client";
 import React from "react";
 import { useSession } from "next-auth/react";
-import { Button } from "@/components/ui/button";
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu";
-import { Avatar, AvatarFallback } from "@/components/ui/avatar";
-
-import { signOut } from "next-auth/react";
-import { ModeToggle } from "../theme/toggle";
-import Nutrients from "../nutrients/Nutrients";
-import { PiSignOut } from "react-icons/pi";
 import { MenubarDemo } from "../menu/MenuBar";
 const Account = () => {
   const { data: session } = useSession();
