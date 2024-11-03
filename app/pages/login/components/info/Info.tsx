7827afa268dbcca52ca49e254a380ab2a5953ede
Cleve Clayton <95036334+manOnTheMoon2101@users.noreply.github.com>
2024-11-03 22:23:43 +0200

    message update

--- app/pages/login/components/info/Info.tsx
+++ app/pages/login/components/info/Info.tsx
@@ -23,8 +23,8 @@ export function Info() {
               Welcome to my Weight Management App!
             </h2>
             <p className="m-5 text-center ">
-              This platform is designed specifically for showcasing individual
-              work and is used by a select group of users.
+              This platform is designed specifically for showcasing some of my
+              skills and is used by a select group of users.
               <br />
               <br />
               To ensure the security of your information, please avoid using
@@ -35,10 +35,9 @@ export function Info() {
             </p>
             <p className="m-5 text-center">
               If you encounter any bugs or issues while using the site,I
-              encourage you to report them so we can address them promptly.
+              encourage you to report them so I can address them promptly.
               <br />
-              Your feedback is invaluable in helping me improve the experience
-              for everyone.
+              Your feedback is invaluable in helping me improve the experience.
             </p>
           </div>
         </DialogContent>
