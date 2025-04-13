e0da3a20d9421f1208b1f297e968c6daf1d22154
Cleve Clayton <95036334+manOnTheMoon2101@users.noreply.github.com>
2025-04-13 18:44:46 +0200

    updated to demo

--- app/demo/page.tsx
+++ app/demo/page.tsx
@@ -1,12 +1,48 @@
-import React from 'react'
-import Header from '../components/header/Header'
+"use client";
+import {
+  Menubar,
+  MenubarContent,
+  MenubarMenu,
+  MenubarTrigger,
+} from "@/components/ui/menubar";
+import { Button } from "@/components/ui/button";
+import { Avatar, AvatarFallback } from "@/components/ui/avatar";
+import { PiSignOut } from "react-icons/pi";
+import { MdAccountCircle } from "react-icons/md";
+import Link from "next/link";
 
 function Demo() {
   return (
     <div>
-      <Header/>
+      <div className="flex flex-row justify-end items-baseline bg-accent">
+        <Menubar>
+          <MenubarMenu>
+            <MenubarTrigger>
+              <MdAccountCircle />
+            </MenubarTrigger>
+            <MenubarContent className="bg-background">
+              <div>
+                <div className="flex flex-row justify-around items-center">
+                  <Avatar className="bg-accent">
+                    <AvatarFallback>D</AvatarFallback>
+                  </Avatar>
+                  <span>Demo</span>
+                </div>
+                <div className="flex flex-row justify-end">
+                  <Button className="bg-red-900 text-slate-50 cursor-pointer">
+                    <Link href="/" className="text-accent">
+                      Log Out
+                    </Link>
+                    <PiSignOut />
+                  </Button>
+                </div>
+              </div>
+            </MenubarContent>
+          </MenubarMenu>
+        </Menubar>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Demo
+export default Demo;
