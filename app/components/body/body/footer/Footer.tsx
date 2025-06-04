5b04542cfe30b286d9c5e021c8503c22aeb9508e
Cleve Clayton <95036334+manOnTheMoon2101@users.noreply.github.com>
2025-06-04 17:16:02 +0200

    footer update

---
+++ app/components/body/body/footer/Footer.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+const Footer = () => {
+  return (
+    <div className="flex flex-row justify-center items-center">
+      <div className="text-center">
+        <div>Designed by @cleveclayton</div>
+        <div>@2025</div>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
